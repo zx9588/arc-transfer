277082af9d07863fcea48ef0bedc0c47b2d17de0
zhangxin <1205362172@qq.coms>
2023-05-19 14:11:15 +0800

    pls

--- xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/HikvisionXxlJob.java
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/service/jobhandler/HikvisionXxlJob.java
@@ -1,36 +1,19 @@
 package com.xxl.job.executor.service.jobhandler;
 
-import com.alibaba.fastjson2.JSONObject;
 import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.annotation.XxlJob;
 import com.xxl.job.executor.mqtt.MqttGateway;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static org.apache.logging.log4j.message.MapMessage.MapFormat.JSON;
 
 /**
- * XxlJob开发示例（Bean模式）
- * <p>
- * 开发步骤：
- * 1、任务开发：在Spring Bean实例中，开发Job方法；
- * 2、注解配置：为Job方法添加注解 "@XxlJob(value="自定义jobhandler名称", init = "JobHandler初始化方法", destroy = "JobHandler销毁方法")"，注解value值对应的是调度中心新建任务的JobHandler属性的值。
- * 3、执行日志：需要通过 "XxlJobHelper.log" 打印执行日志；
- * 4、任务结果：默认任务结果为 "成功" 状态，不需要主动设置；如有诉求，比如设置任务结果为失败，可以通过 "XxlJobHelper.handleFail/handleSuccess" 自主设置任务结果；
- *
- * @author xuxueli 2019-12-11 21:52:51
+ * @author zhangxin
  */
 @Component
+@Slf4j
 public class HikvisionXxlJob {
-    private static Logger logger = LoggerFactory.getLogger(HikvisionXxlJob.class);
 
 //    @Resource
 //    DeviceService deviceService;
